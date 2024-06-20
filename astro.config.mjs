bd7f30509f6dc192ea09855471b0f0ddf80c0bdf
doiska <doiska@proton.me>
2024-06-20 14:02:51 -0300

    build: vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,21 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    output: "server",
-    devToolbar: {
-        enabled: false
-    },
-    site: 'https://twokei.com',
-    integrations: [
-        mdx({
-            shikiConfig: {
-                theme: "vitesse-dark"
-            }
-        }),
-        sitemap(),
-        tailwind()
-    ]
+  output: "server",
+  devToolbar: {
+    enabled: false
+  },
+  site: 'https://twokei.com',
+  integrations: [mdx({
+    shikiConfig: {
+      theme: "vitesse-dark"
+    }
+  }), sitemap(), tailwind()],
+  adapter: vercel()
 });
